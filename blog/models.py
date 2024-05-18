e0fbe5da469dd23a2bf8340445e858b094475dbb
nWaldi <ppmip2@gmail.com>
2024-05-18 17:46:18 +0200

    added paginatiom feature

--- blog/models.py
+++ blog/models.py
@@ -1,17 +1,18 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
-        return super().get_queryset().filter(status=Post.Status.PUBLUSHED)
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = "DF", "roboczy"
         PUBLISHED = "PB", "Opublikowany"
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True, unique_for_date="publish")
     author = models.ForeignKey(User,
                                on_delete=models.CASCADE,
                                related_name='blog_posts')
@@ -32,3 +33,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year,
+                                                 self.publish.month,
+                                                 self.publish.day,
+                                                 self.slug])
