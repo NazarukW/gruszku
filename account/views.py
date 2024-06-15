58b6a08d1106b0fa41ca15f97de751d79e4e47fc
nWaldi <ppmip2@gmail.com>
2024-06-15 18:02:44 +0200

    added login and logout view for account and dashboard template

--- account/views.py
+++ account/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
-
+from django.contrib.auth.decorators import login_required
 from account.forms import LoginForm
 
 
@@ -24,3 +24,9 @@ def user_login(request):
     else:
         form = LoginForm()
     return render(request, "account/login.html", {'form': form})
+
+@login_required
+def dashboard(request):
+    return render(request,
+                  "account/dashboard.html",
+                  {"section": "dashboard"})
